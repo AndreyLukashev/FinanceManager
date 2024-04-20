9dbf8189886e02a74b826cff52789a466e696e4a
AndreyLukashev <energy2024@internet.ru>
2024-04-20 07:16:58 +0300

    add routing

--- main.js
+++ main.js
@@ -1,2 +1,9 @@
 import "./style.css";
 import "lucide-static/font/Lucide.css";
+
+import "./src/app.component";
+
+const root = document.getElementById("app");
+const app = document.createElement("my-app");
+
+root.append(app);
---
+++ src/constants/routes.js
@@ -0,0 +1,7 @@
+export const ROUTES = {
+    home: "/",
+    signUp: "/sign-up",
+    signIn: "/sign-in",
+    work: "/work",
+    notFound: "/404",
+}
