7bd6b25cc8ee13e4222a06ba6443abd262dfc137
AndreyLukashev <energy2024@internet.ru>
2024-04-20 08:42:33 +0300

    add sign-in sign-up template

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,hbs}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        workspace:
+          'url("https://images.unsplash.com/photo-1579621970588-a35d0e7ab9b6?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")',
+      },
+    },
   },
   plugins: [require("tailwindcss-animated")],
 }
