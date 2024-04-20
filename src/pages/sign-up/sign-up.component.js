9f3832a11cb75c83c93b975bac88069d4754867c
AndreyLukashev <energy2024@internet.ru>
2024-04-20 09:37:01 +0300

    add sign-up user

--- src/pages/sign-up/sign-up.component.js
+++ src/pages/sign-up/sign-up.component.js
@@ -1,11 +1,11 @@
 import { Component } from "../../core/Component";
 import template from "./sign-up.template.hbs";
 import { ROUTES } from "../../constants/routes";
-// import { extractFormData } from "../../utils/extractFormData";
-// import { authService } from "../../services/Auth";
-// import { useToastNotification } from "../../hooks/useToastNotification";
-// import { TOAST_TYPE } from "../../constants/toast";
-// import { useNavigate } from "../../hooks/useNavigate";
+import { extractFormData } from "../../utils/extractFormData";
+import { authService } from "../../services/Auth";
+import { useToastNotification } from "../../hooks/useToastNotification";
+import { TOAST_TYPE } from "../../constants/toast";
+import { useNavigate } from "../../hooks/useNavigate";
 // import { useUserStore } from "../../hooks/useUserStore";
 
 export class SignUp extends Component {
@@ -18,43 +18,43 @@ export class SignUp extends Component {
     };
   }
 
-//   toggleIsLoading = () => {
-//     this.setState({
-//       ...this.state,
-//       isLoading: !this.state.isLoading,
-//     });
-//   };
+  toggleIsLoading = () => {
+    this.setState({
+      ...this.state,
+      isLoading: !this.state.isLoading,
+    });
+  };
 
-//   registerUser = (evt) => {
-//     evt.preventDefault();
-//     const formData = extractFormData(evt.target);
-//     this.toggleIsLoading();
+  registerUser = (evt) => {
+    evt.preventDefault();
+    const formData = extractFormData(evt.target);
+    this.toggleIsLoading();
 //     const { setUser } = useUserStore();
-//     authService
-//       .signUp(formData.email, formData.password)
-//       .then((data) => {
+    authService
+      .signUp(formData.email, formData.password)
+      .then((data) => {
 //         setUser({ ...data.user });
-//         useToastNotification({
-//           message: "Success!!!",
-//           type: TOAST_TYPE.success,
-//         });
-//         useNavigate(ROUTES.work);
-//       })
-//       .catch((error) => {
-//         useToastNotification({ message: error.message });
-//       })
-//       .finally(() => {
-//         this.toggleIsLoading();
-//       });
-//   };
+        useToastNotification({
+          message: "Success!!!",
+          type: TOAST_TYPE.success,
+        });
+        useNavigate(ROUTES.work);
+      })
+      .catch((error) => {
+        useToastNotification({ message: error.message });
+      })
+      .finally(() => {
+        this.toggleIsLoading();
+      });
+  };
 
-//   componentDidMount() {
-//     this.addEventListener("submit", this.registerUser);
-//   }
+  componentDidMount() {
+    this.addEventListener("submit", this.registerUser);
+  }
 
-//   componentWillUnmount() {
-//     this.removeEventListener("submit", this.registerUser);
-//   }
+  componentWillUnmount() {
+    this.removeEventListener("submit", this.registerUser);
+  }
 }
 
 customElements.define("sign-up-page", SignUp);
