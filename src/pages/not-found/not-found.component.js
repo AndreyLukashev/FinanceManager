e5a93d6b45edb2d2a4ca5c2bbd9271ada8fc1e35
AndreyLukashev <energy2024@internet.ru>
2024-04-21 08:19:21 +0300

    add template notfound

--- src/pages/not-found/not-found.component.js
+++ src/pages/not-found/not-found.component.js
@@ -1,4 +1,6 @@
+import { ROUTES } from "../../constants/routes";
 import { Component } from "../../core/Component";
+import { useNavigate } from "../../hooks/useNavigate";
 import template from "./not-found.template.hbs";
 
 export class NotFound extends Component {
@@ -6,6 +8,23 @@ export class NotFound extends Component {
     super();
     this.template = template();
     this.state = {};
+  }
+
+  onClick = ({target}) => {
+    const homePage = target.closest('.home-page');
+  
+    if(homePage){
+      useNavigate(`${ROUTES.home}`);
+    }
+
+  }
+
+  componentDidMount() {
+    this.addEventListener('click', this.onClick);
+  }
+
+  componentWillUnmount() {
+    this.removeEventListener('clik', this.onClick)
   }
 }
 
